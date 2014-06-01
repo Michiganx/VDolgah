25d162aaa8a1707835d6131e98fa1b104d3d3622
Eric <michigan@ukr.net>
2014-06-01 22:19:22 +0300

    Create Solution Create DB

---
+++ VDolgah/VDolgah/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VDolgah.MvcApplication" Language="C#" %>
---
+++ VDolgah/VDolgah/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VDolgah.Startup))]
+namespace VDolgah
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
