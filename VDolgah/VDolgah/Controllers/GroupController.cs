dee1b714f260dfd3a05327adabede4979c3440e0
andrii1812 <andrii1812@gmail.com>
2014-09-28 14:57:13 +0300

    Revert ""modified" bug"
    This reverts commit 4a7fe6509abde4703a15af74bcf58ae958748557.

--- VDolgah/VDolgah/Controllers/GroupController.cs
+++ VDolgah/VDolgah/Controllers/GroupController.cs
@@ -96,7 +96,6 @@ namespace VDolgah.Controllers
                     d.value = debt;
                     db.debts.Add(d);
                 }
-                db.Entry(user).State = System.Data.EntityState.Modified;
             }
             db.SaveChanges();
             return RedirectToAction("Index", new { group_id = group_id });
@@ -106,7 +105,6 @@ namespace VDolgah.Controllers
         {
             VDolgah.Models.Minimizer min = new Minimizer();
             min.MinGroup(group_id);
-            db.Entry(db.groups.Where((x) => x.idgroups == group_id)).State = System.Data.EntityState.Modified;
             return RedirectToAction("Index", new { group_id = group_id });
         }
 
